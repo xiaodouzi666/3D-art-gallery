ccd97628f8afd9e9ac1cd8c41b6e29453bbd16fd
xiaodouzi <77219630+xiaodouzi666@users.noreply.github.com>
2023-10-03 15:14:38 +0800

    Update paintings.js

--- modules/paintings.js
+++ modules/paintings.js
@@ -1,3 +1,20 @@
+
+async function fetchFromWukongHuaHua(style, desc) {
+    let response = await fetch('https://xihe.mindspore.cn/api/v1/bigmodel/api/wukong', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': '23cd2ad70515287fdb06f85b1a1bf0ad54a4267e695e92ca91a5b08a9f6dec0c015b7e853afc93672ef8cfecbec8ae'
+        },
+        body: JSON.stringify({
+            "style": style,
+            "desc": desc
+        })
+    });
+    
+    let data = await response.json();
+    return data.data.pictures[0];
+}
 import * as THREE from 'three';
 
 import { paintingData } from './paintingData.js';
@@ -8,6 +25,11 @@ export function createPaintings(scene, textureLoader) {
 
   paintingData.forEach((data) => {
    
+    
+    const style = prompt("请输入图片风格描述 (e.g., 赛博朋克): ");
+    const desc = prompt("请输入图片描述 (e.g., 城市夜景): ");
+    const imgSrc = await fetchFromWukongHuaHua(style, desc);
+    
     const painting = new THREE.Mesh( 
       new THREE.PlaneGeometry(data.width, data.height),
       new THREE.MeshLambertMaterial({ map: textureLoader.load(data.imgSrc) })
